30539384d5c8c885871d58c796e5e8938168c564
Marco Spedaletti <asimov@mclink.it>
2020-10-16 00:56:41 +0200

    Minor fix

--- src/midres_vic20.c
+++ src/midres_vic20.c
@@ -293,6 +293,8 @@ void mr_end_frame_hd(unsigned char _jiffies) {
 
 }
 
+#ifdef MIDRES_STANDALONE_FILE
+
 unsigned char* mr_translate_file_hd(mr_file _file) {
     return mr_translate_file_user(_file);
 }
@@ -318,4 +320,5 @@ void mr_read_file_hd(unsigned int _file, unsigned int _offset, unsigned char* _d
     fclose(f);
 }
 
+#endif
 #endif
