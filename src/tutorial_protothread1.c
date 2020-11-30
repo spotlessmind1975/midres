630d654717abd035bf17c70e86164ad594143494
Marco Spedaletti <asimov@mclink.it>
2020-11-30 23:22:48 +0100

    Protothreads: added example for introductive article.

---
+++ src/tutorial_protothread1.c
@@ -0,0 +1,68 @@
+#define __TUTORIAL__PROTOTHREAD1__ 1
+#ifdef __TUTORIAL__PROTOTHREAD1__
+
+/****************************************************************************
+ * midres - Portable midres library for retrocomputers                      *
+ *                                                                          *
+ * Copyright (c) 2020 by Marco Spedaletti. Licensed under CC-BY-NC-SA       *
+ *--------------------------------------------------------------------------*
+ * RESIDENT MODULE                                                          *
+ ****************************************************************************/
+
+ /****************************************************************************
+  ** INCLUDE SECTION
+  ****************************************************************************/
+
+  // Needed for system call.
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+// Needed for MIDRES library call.
+#include "midres.h"
+
+MR_PT_CTX(pippo, unsigned char variable)
+
+mr_protothread firstThread;
+mr_protothread secondThread;
+
+MR_PT_THREAD(first) {
+
+	MR_PTI_BEGIN();
+
+	while (1) {
+
+		printf("Hello world 1\n");
+
+		MR_PTI_YIELD();
+
+	}
+
+	MR_PTI_END();
+}
+
+MR_PT_THREAD(second) {
+
+	MR_PTI_BEGIN();
+
+	while (1) {
+
+		printf("Hello world 2\n");
+
+		MR_PTI_YIELD();
+
+	}
+
+	MR_PTI_END();
+}
+
+void tutorial_protothread1() {
+
+	while (1) {
+		first(&firstThread);
+		second(&secondThread);
+	}
+
+}
+
+#endif
